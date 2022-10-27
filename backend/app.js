00646935ef276f869874ebde535e324e2c351bc4
WILLSON <98232630+CAMEROU@users.noreply.github.com>
2022-10-27 01:55:45 -0700

    modification

--- backend/app.js
+++ backend/app.js
@@ -3,33 +3,9 @@ const  { saucesRouter } = require("./routers/saucesrouter")
 const  { authRouter } = require("./routers/authrouter")
 const bodyParser = require("body-parser")
 const path = require("path")
-const helmet = require("helmet")
 const port = 3000
 //Connexion a la base de donnee
 require("./mongo")
-
-// Middleware
-/*app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: [
-          "'self'",
-          "https://www.googletagmanager.com",
-          "'self'",
-          "https://www.google-analytics.com",
-          "'unsafe-inline'",
-          "mydomain.com",
-        ],
-        imgSrc: ["'self'", "assets.mydomain.com"],
-      },
-    },
-    crossOriginEmbedderPolicy: false,
-    crossOriginResourcePolicy: false,
-  })
-);*/
-
 
 app.use(bodyParser.json())
 //Gestion des principaux routes
