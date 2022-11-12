036cf73d64cc782a43f8bcbc35ea0bf5f5c3f445
WILLSON <98232630+CAMEROU@users.noreply.github.com>
2022-11-11 17:02:19 -0800

    correction dans users.js

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -3,30 +3,27 @@
   const bcrypt = require("bcrypt")
   // Importation du package de jsonwebtoken
   const jwt = require("jsonwebtoken")
- const mailRegex = require("../mailRegex")
+  let {mailRegex} = require("../mailRegex")
 
 async  function createUser(req, res) {
     try{
       const {email, password} = req.body
       if (!email || email.length === 0) return res.status(400).send({message: "votre  e-mail doit être  défini" })
       if (!email.match(mailRegex)) return res.status(400).send({message: "votre email n'est pas au bon format" })
-      if(!email || email.length === 0) return res.status(400).send({message: "votre e-mail doit être défini" })
       if (!password || password.length === 0) return res.status(400).send({message: "votre mot de passe doit être défini" })
       if (password.length < 8) return  res.status(400).send({message: "votre mot de passe doit contenir au minimum 8 caractères" })
-
-    const hashedPassword = await hashPassword(password)
-    const user = new User({email, password: hashedPassword })
-    await user.save()
-    res.status(201).send({message: "Utilisateur enregistre !" })
-    }catch (err){
-    res.status(409).send({message: "User non enregistre : " + err })}
-}
+      const hashedPassword = await hashPassword(password)
+      const user = new User({email, password: hashedPassword })
+      await user.save()
+      res.status(201).send({message: "Utilisateur enregistré !" })
+      }catch (err){
+      res.status(409).send({message: "Utilisateur non enregistré : " + err })}
+   }
 
 function hashPassword(password) {
-    const saltRounds = 10
-    return bcrypt.hash(password, saltRounds)
+      const saltRounds = 10
+      return bcrypt.hash(password, saltRounds)
  }
-
 //  fonction asynchrone qui renvoie une Promise dans laquelle nous recevons le hash généré ;
 async function logUser(req, res) {
     try {
--- backend/mailRegex.js
+++ backend/mailRegex.js
@@ -1,3 +1,2 @@
-const mailRegex = /^[a-zA-Z]+[a-zA-Z0-9_.]+@[a-zA-Z.]+[a-zA-Z]$/;
-
+ const mailRegex = /^[a-zA-Z]+[a-zA-Z0-9_.]+@[a-zA-Z.]+[a-zA-Z]$/;
 module.export = { mailRegex}
